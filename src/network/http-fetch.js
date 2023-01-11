a8be99003177f1576ba79b80065b545c750034d1
parkmyungwan <mywanpark@gmail.com>
2023-01-11 19:43:06 +0900

    Feat: use Axios for specific retry config when getting 429 status code

---
+++ src/network/http-fetch.js
@@ -0,0 +1,36 @@
+export default class HttpClient {
+  constructor(baseURL, authErrorEventBus, getCsrfToken) {
+    this.baseURL = baseURL;
+    this.authErrorEventBus = authErrorEventBus;
+    this.getCsrfToken = getCsrfToken;
+  }
+  async fetch(url, options) {
+    const res = await fetch(`${this.baseURL}${url}`, {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.headers,
+        'twitter-csrf-token': this.getCsrfToken(),
+      },
+      credentials: 'include',
+    });
+
+    let data;
+    try {
+      data = await res.json();
+    } catch (error) {
+      console.error(error);
+    }
+    if (res.status > 299 || res.status < 200) {
+      const message =
+        data && data.message ? data.message : 'something went wrong!!!';
+      const error = new Error(message);
+      if (res.status === 401) {
+        this.authErrorEventBus.notify(error);
+        return;
+      }
+      throw error;
+    }
+    return data;
+  }
+}
--- src/network/http.js
+++ src/network/http.js
@@ -1,36 +1,61 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+const defaultRetryConfig = {
+  retries: 5,
+  initialDelayMS: 100,
+};
+
 export default class HttpClient {
-  constructor(baseURL, authErrorEventBus, getCsrfToken) {
-    this.baseURL = baseURL;
+  constructor(
+    baseURL,
+    authErrorEventBus,
+    getCsrfToken,
+    config = defaultRetryConfig
+  ) {
+    this.config = config;
+    this.client = axios.create({
+      baseURL: baseURL,
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    });
+    axiosRetry(this.client, {
+      retries: this.config.retries,
+      retryDelay: (retry) => {
+        const delay = Math.pow(2, retry) * this.config.initialDelayMS;
+        const jitter = delay * 0.1 * Math.random();
+        return delay + jitter;
+      },
+      retryCondition: (err) =>
+        axiosRetry.isNetworkOrIdempotentRequestError(err) ||
+        err.response.status === 429,
+    });
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
   }
   async fetch(url, options) {
-    const res = await fetch(`${this.baseURL}${url}`, {
-      ...options,
+    const { body, method, headers } = options;
+    const request = {
+      url,
+      method,
       headers: {
-        'Content-Type': 'application/json',
-        ...options.headers,
+        ...headers,
         'twitter-csrf-token': this.getCsrfToken(),
       },
-      credentials: 'include',
-    });
+      data: body,
+    };
 
-    let data;
     try {
-      data = await res.json();
-    } catch (error) {
-      console.error(error);
-    }
-    if (res.status > 299 || res.status < 200) {
-      const message =
-        data && data.message ? data.message : 'something went wrong!!!';
-      const error = new Error(message);
-      if (res.status === 401) {
-        this.authErrorEventBus.notify(error);
-        return;
+      const response = await this.client(request);
+      return response.data;
+    } catch (err) {
+      if (err.response) {
+        const data = err.response.data;
+        const message =
+          data && data.message ? data.message : 'something went wrong!!!';
+        throw new Error(message);
       }
-      throw error;
+      throw new Error('connection error');
     }
-    return data;
   }
 }
